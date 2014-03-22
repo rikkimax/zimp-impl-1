04f23fee75f2f92c969fe651cd4eec9e2b178fc9
Loki Kristianson <lokik@live.com>
2014-03-22 13:45:29 +1300

    Create defs.py

---
+++ defs.py
@@ -0,0 +1,30 @@
+__author__ = 'Rikki'
+
+
+class Direction:
+
+
+    Unknown = 1 << 0
+    UP = 1 << 1
+    DOWN = 1 << 2
+    LEFT = 1 << 3
+    RIGHT = 1 << 4
+
+
+class Tiles:
+    Foyer = 0
+    Patio = 1
+    EvilTemple = 2
+    StorageRoom = 3
+    Kitchen = 4
+    DiningRoom = 5
+    FamilyRoom = 6
+    Bedroom = 7
+    Bathroom = 8
+    Garden = 9
+    Graveyard = 10
+    Garage = 11
+    SittingArea = 12
+    Yard1 = 13
+    Yard2 = 14
+    Yard3 = 15
